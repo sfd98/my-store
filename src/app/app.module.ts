78520169177133be97a22c523b09854a61704d4f
sfd98 <samueldelgado98@gmail.com>
2022-11-03 15:30:36 +0100

    first scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemDetailComponent,
+    ProductItemComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule,
